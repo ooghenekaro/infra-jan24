2f92eaf1cfcf35f14fb64d0d4078f6096083b618
ooghenekaro <114274865+ooghenekaro@users.noreply.github.com>
2024-02-11 21:34:59 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
 
+terraform {
+  backend "s3" {
+    bucket = "karo-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+    dynamo_table = "terraform-state"
+    encrypt = true
+  }
+}
+
 
 # Configure the AWS Provider
 provider "aws" {
